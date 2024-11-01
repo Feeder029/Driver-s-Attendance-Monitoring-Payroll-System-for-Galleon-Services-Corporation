ffce937d2c411438ba5c49e15eb09d1e64a42346
Feeder1129 <mrmm112903@gmail.com>
2024-11-01 23:55:33 +0800

    ACCOUNTS LIST ACCOUNTS

--- Code Files/JS Files/AdminDashboard.js
+++ Code Files/JS Files/AdminDashboard.js
@@ -1,3 +1,13 @@
 window.onload = function() {
     document.getElementById('iframe-dashboard').src = 'Accounts.html';
 };
+
+
+const sidebarLinks = document.querySelectorAll('.side-menu li a');
+
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        sidebarLinks.forEach(l => l.classList.remove('active')); 
+        this.classList.add('active');
+    });
+});

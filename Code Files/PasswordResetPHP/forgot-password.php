d874f4e9a2c6cbbaac53922fda208d99d0e5faf4
Jarencio <145330400+Jarencio@users.noreply.github.com>
2024-11-18 19:49:55 +0800

    Forgot Password Pre Design

--- Code Files/PasswordResetPHP/forgot-password.php
+++ Code Files/PasswordResetPHP/forgot-password.php
@@ -4,17 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORGOT PASSWORD</title>
+    <link rel="stylesheet" href="../CSS Files/ForgotPassword.css">
 </head>
 <body>
-    <H1>FORGOT PASSWORD</H1>
 
+
+    <div class="container">
     <form action="send-password-reset.php" method="post">
 
-    <Label for="Email">EMAIL:</Label>
-    <input type="Email" name="Email" id="Email">
-    <button> Send </button>
+    <h1> RESET PASSWORD </h1>
+    <hr class="line">
+    <p class="desc"> Please enter the email address associated with your account. Well send you an email with a link to reset your password.</p>
+
+    <div class="containers">
+    <Label for="Email" id="emaillabel">EMAIL ADDRESS:</Label>
+    <input type="email" name="Email" id="Email" class="textbox_email">
+    <button class="buttons"> Submit </button>
+    </div>
 
     </form>
+    </div>
+
     
 </body>
 </html>
